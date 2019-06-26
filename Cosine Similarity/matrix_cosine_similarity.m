a4d0b6f1dec885802091c925078a851cfc705291
yacine <yacine.mahdid@mail.mcgill.ca>
2019-06-26 09:44:04 -0400

    refactor and document

--- Cosine Similarity/matrix_cosine_similarity.m
+++ Cosine Similarity/matrix_cosine_similarity.m
@@ -6,28 +6,31 @@ function [similarities] = matrix_cosine_similarity(A,B)
 %
 %   similarities is a matrix of size L*M which represent the similarity of all dimension (L and M) of A and B
 %   ranging from [0,1]
+    
+    %% Variable Initalization
+    number_row_a = size(A,1);
+    number_row_b = size(B,1);
+    number_col_a = size(A,2);
+    number_col_b = size(B,2);
 
+    similarities = zeros(number_row_a,number_row_b);
     
     %% Error checking
     % Check if we have the same number of columns
-    if(size(A,2) ~= size(B,2))
+    if(number_col_a ~= number_col_b)
        error("The matrix A and the matrix B need tbe of same lenght.");
     end
     
     % It is allowed to have different number of rows as we will get a
     % similarities matrix of length_row_a*length_row_b
     
-    %% Iterate over one dimension
-    dimension_a = size(A,1);
-    dimension_b = size(B,1);
-    similarities = zeros(dimension_a,dimension_b);
-    
-    for i = 1:dimension_a
-        a = A(i);
-        for j = 1:dimension_b
-            b = B(j);
+    %% Iterate over the row and calculate vector cosine similarities
+    for row_a_i = 1:number_row_a
+        a = A(row_a_i);
+        for row_b_i = 1:number_row_b
+            b = B(row_b_i);
             similarity = vector_cosine_similarity(a,b);
-            similarities(i,j) = similarity;
+            similarities(row_a_i,row_b_i) = similarity;
         end
     end
     
