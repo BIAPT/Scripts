be349feaa518083e7cc369c6930fa8e9464d8214
yacine <yacine.mahdid@mail.mcgill.ca>
2019-06-26 10:08:21 -0400

    add two preprocessing functions

---
+++ Cosine Similarity/get_posterior.m
@@ -0,0 +1,11 @@
+function [sifted_vector] = get_posterior(vector,channels_location)
+%GET_ANTERIOR Summary of this function goes here
+%   Detailed explanation goes here
+
+    sifted_vector = []; %% TODO check how many are smaller than -0.001
+    for i = 1:length(vector)
+        if(channels_location(i).X < 0.001)
+            sifted_vector = [sifted_vector, vector(i)];
+        end
+    end
+end
