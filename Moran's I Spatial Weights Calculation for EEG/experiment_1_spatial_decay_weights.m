a07c6c82cca5fd769a4f5dbf552003b66224f51b
Bl0101 <yacine.mahdid@live.com>
2019-08-01 12:11:55 -0400

    Moran's I Spatial weights for EEG

---
+++ Moran's I Spatial Weights Calculation for EEG/experiment_1_spatial_decay_weights.m
@@ -0,0 +1,41 @@
+%% Spatial Weights Matrix Calculation
+% Here we will use euclidian spatial decay to create our weight matrix
+% To start we will calcualte it on one eeg location file
+
+%% Load the data
+data = load('EEG_info_WSAS13.mat');
+channel_location = data.EEG_info.chanlocs;
+
+%% Init the weight matrix
+num_channels = length(channel_location);
+weight_matrix = zeros(num_channels,num_channels);
+
+%% Populating the Weights matrix
+for i = 1:num_channels
+    first_point = [channel_location(i).X, channel_location(i).Y, channel_location(i).Z];
+    for j = 1:num_channels
+        second_point = [channel_location(j).X, channel_location(j).Y, channel_location(j).Z];
+        weight_matrix(i,j) = euclidean_distance(first_point,second_point);
+    end
+end
+
+%% Normalization with min max
+weight_matrix = min_max_normalization(weight_matrix); % This give use normalized distance
+weight_matrix = abs(1 - weight_matrix); % This give us normalized proximity
+
+
+%% Helper functions
+function distance = euclidean_distance(first_point, second_point)
+    distance = sqrt(sum((first_point - second_point) .^ 2));
+end
+
+function normalized_matrix = min_max_normalization(matrix)
+    normalized_matrix = (matrix - min(matrix)) ./ (max(matrix) - min(matrix));
+    
+    % Restore the symmetry
+    for i = 1:length(matrix)
+       for j = 1:length(matrix)
+          normalized_matrix(i,j) = normalized_matrix(j,i); 
+       end
+    end
+end
