d7a0e6767738098e57faac2dce45d03deefa3c6d
Bl0101 <yacine.mahdid@live.com>
2019-08-02 15:49:44 -0400

    add all_brain_plotting script
    This script makes use of previous code that catherine used.
    It was heavily edited in order to be able to show colormap based on vector of weigths. It's currently set up in such a way that it accepts 4 colors.

---
+++ AEC and wPLI comparison/plot/aal_brain_plotting.m
@@ -0,0 +1,83 @@
+%% Variables initialization
+% Weigts
+data = load('weights_colors.mat');
+weights_pli_pre_ROC = data.weights_pli_pre_ROC;
+weights_aec_pre_ROC = data.weights_aec_pre_ROC;
+weights_pli_unconscious = data.weights_pli_unconscious;
+weights_aec_unconscious = data.weights_aec_unconscious;
+
+% Colormap [nothing = 1, mean = 2, std = 3, both= 4]
+color_map = [ 1 1 1 ; 0.4660, 0.6740, 0.1880 ; 0.9290, 0.6940, 0.1250; 0, 0.4470, 0.7410]; % This is the colormap for the not important, mean, std, both
+
+% Side of brain to map (left or right)
+side = 'right';
+if(strcmp(side,'left'))
+   roi_range = 1:41;
+elseif(strcmp(side,'right'))
+    roi_range = 42:82;
+end
+
+%% Weights vector initialization
+weights = weights_pli_unconscious; %To change using the values above
+
+%% Load the necessary variables
+load('ROI_MNI_V5_nii.mat'); % AALreg = volume image of 120 ROIs
+load('ROI_MNI_V5_vol.mat'); % ROI1 = contains info on ROIs
+load('Atlas_scouts.mat'); % scout = info of edited atlas 
+
+%% Set the scouts name and labels
+scout = scout(roi_range);
+
+number_locs = length(scout); % number_locs =  82
+
+% Matches scout ROIs to original AAL atlas
+labellist = zeros(1,number_locs);
+
+for jj = 1:length(scout)
+        ll = scout(jj).Label;
+        ll(end-1)='_';
+    for ii = 1:length(ROI1)
+        if strcmp(ROI1(ii).Nom_L,ll)
+            labellist(jj) = ii;
+        end
+    end
+end
+
+%% Fix the ROI
+roi = zeros([size(AALreg),number_locs]);
+for reg = 1:number_locs
+    roi(:,:,:,reg) = AALreg == ROI1(labellist(reg)).ID;
+end
+
+%% Plot the brain with the correct weights
+% Can specify colour limits, here 0 to 1
+aal_brain(roi_range,weights,roi,color_map);
+camlight (90,30)
+set(gca,'view',[270 90]); %Changes angle of view
+
+
+
+%% Helper function
+function aal_brain(roi_range,weights,roi,color_map)
+    % fc = functional connectivity matrix or vector
+    % roi = volume matrix of ROI regions, 3dims x no. ROIs  
+    
+    % Create the figure
+    figure
+    axis ([0,100,0,50,0,100])
+    c = colormap(color_map);
+    hold all
+
+    colorindex = weights(roi_range);
+
+    for reg = 1:length(roi_range)
+        roisurf=isosurface(roi(:,:,:,reg),0.5);
+        h = trisurf(roisurf.faces,roisurf.vertices(:,1),roisurf.vertices(:,2),roisurf.vertices(:,3));
+        set(h,'facecolor',c(colorindex(reg),:),'facealpha',0.8,'LineWidth',0.1,'LineStyle','none');
+    end
+
+    set(gca,'view',[-90 90])
+    axis equal
+    axis off
+    set(gcf,'color','white')
+end
---
+++ Moran's I Spatial Weights Calculation for EEG/experiment_4_moran_s_I_coding.m
@@ -0,0 +1,23 @@
+function I = myMoransI(grid,weight_matrix)
+    if (size(grid,1) > 1)
+        x = reshape(grid,[1 size(weight_matrix,1)]);
+    else
+        x = grid;
+    end
+    
+    
+    % TODO change these two to not take the NAN into consideration
+    N = size(weight_matrix,1);
+    W = sum(sum(weight_matrix));
+    
+    Xbar = nanmean(grid);
+    num = 0;
+    denom = 0;
+    for i = 1:size(weight_matrix,1)
+        for j = 1:size(weight_matrix,2)
+            num = nansum([num weight_matrix(i,j)*(x(i)-Xbar)*(x(j)-Xbar)]);
+        end
+        denom = nansum([denom (x(i)-Xbar)^2]);
+    end
+    I = (N/W)*(num/denom);
+end
