759a887d2132c6301ecbe05230ca6d1effdadd2b
dnadin <danielle.nadin@mail.mcgill.ca>
2019-08-02 15:44:11 -0400

    Create compute_moransI.m

---
+++ Moran's I Spatial Weights Calculation for EEG/compute_moransI.m
@@ -0,0 +1,28 @@
+%% Compute Moran's I
+
+% Ignore NaN values in the input grid
+
+function I = compute_moransI(grid,weight_matrix)
+if (size(grid,1) > 1)
+    x = reshape(grid,[1 size(weight_matrix,1)]);
+else
+    x = grid;
+end
+ind = find(~isnan(x)); %indices of non-NaN elements in grid
+N = size(ind,2);
+W = 0;
+Xbar = nanmean(x);
+num = 0;
+denom = 0;
+for i = 1:size(weight_matrix,1)
+    for j = 1:size(weight_matrix,2)
+        num = nansum([num weight_matrix(i,j)*(x(i)-Xbar)*(x(j)-Xbar)]);
+        if ~isnan(x(i)) && ~isnan(x(j))
+            W = W + weight_matrix(i,j);
+        end
+    end
+    denom = nansum([denom (x(i)-Xbar)^2]);
+end
+I = (N/W)*(num/denom);
+end
+
