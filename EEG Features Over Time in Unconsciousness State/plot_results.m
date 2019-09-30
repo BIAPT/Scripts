8606411f3b8764de73cb07bd761cf85d736223d7
Yacine <yacine.mahdid@mail.mcgill.ca>
2019-09-30 14:50:22 -0400

    add results for the rest of the plot

--- EEG Features Over Time in Unconsciousness State/plot_results.m
+++ EEG Features Over Time in Unconsciousness State/plot_results.m
@@ -64,101 +64,111 @@ title(strcat(title_name,' PAC RPT Parietal'));
 ylim([min_point, max_point]);
 xlabel('Seconds');
 
-return;
+
 % TODO : CONTINUE THE ANALYSIS!
 % FP wPLI
-avg_left_lateral_wpli = wsas.result_wpli.data.avg_left_lateral_wpli;
-avg_left_midline_wpli = wsas.result_wpli.data.avg_left_midline_wpli;
-avg_right_lateral_wpli = wsas.result_wpli.data.avg_right_lateral_wpli;
-avg_right_midline_wpli = wsas.result_wpli.data.avg_right_midline_wpli;
+avg_left_lateral_wpli = wsas.result_wpli.data.avg_left_lateral_wpli';
+avg_left_midline_wpli = wsas.result_wpli.data.avg_left_midline_wpli';
+avg_right_lateral_wpli = wsas.result_wpli.data.avg_right_lateral_wpli';
+avg_right_midline_wpli = wsas.result_wpli.data.avg_right_midline_wpli';
 
 figure;
-min_point = min([avg_left_midline_wpli avg_left_lateral_wpli, avg_right_midline_wpli, avg_right_lateral_wpli]);
-max_point = max([avg_left_midldine_wpli avg_left_lateral_wpli, avg_right_midline_wpli, avg_right_lateral_wpli]);
+min_point = min([avg_left_midline_wpli, avg_left_lateral_wpli, avg_right_midline_wpli, avg_right_lateral_wpli]);
+max_point = max([avg_left_midline_wpli, avg_left_lateral_wpli, avg_right_midline_wpli, avg_right_lateral_wpli]);
 
 subplot(2,2,1);
-x_index = linspace(0,total_x,length(fp_wpli.left_midline));
-plot(x_index, fp_wpli.left_midline, 'LineWidth',3);
-title('wPLI Left Midline');
+x_index = linspace(0,total_x,length(avg_left_midline_wpli));
+plot(x_index, avg_left_midline_wpli, 'LineWidth',3);
+title(strcat(title_name, ' wPLI Left Midline'));
 ylim([min_point,max_point]);
 xlabel('Seconds');
 
 subplot(2,2,2);
-x_index = linspace(0,total_x,length(fp_wpli.right_midline));
-plot(x_index, fp_wpli.right_midline, 'LineWidth',3);
-title('wPLI Right Midline');
+x_index = linspace(0,total_x,length(avg_right_midline_wpli));
+plot(x_index, avg_right_midline_wpli, 'LineWidth',3);
+title(strcat(title_name, ' wPLI Right Midline'));
 ylim([min_point,max_point]);
 xlabel('Seconds');
 
 subplot(2,2,3);
-x_index = linspace(0,total_x,length(fp_wpli.left_lateral));
-plot(x_index, fp_wpli.left_lateral, 'LineWidth',3);
-title('wPLI Left Lateral');
+x_index = linspace(0,total_x,length(avg_left_lateral_wpli));
+plot(x_index, avg_left_lateral_wpli, 'LineWidth',3);
+title(strcat(title_name, ' wPLI Left Lateral'));
 ylim([min_point,max_point]);
 xlabel('Seconds');
 
 subplot(2,2,4);
-x_index = linspace(0,total_x,length(fp_wpli.right_lateral));
-plot(x_index, fp_wpli.right_lateral, 'LineWidth',3);
-title('wPLI Right Lateral');
+x_index = linspace(0,total_x,length(avg_right_lateral_wpli));
+plot(x_index, avg_right_lateral_wpli, 'LineWidth',3);
+title(strcat(title_name, ' wPLI Right Lateral'));
 ylim([min_point,max_point]);
 xlabel('Seconds');
+
 
 % FP dPLI
+avg_left_lateral_dpli = wsas.result_dpli.data.avg_left_lateral_dpli';
+avg_left_midline_dpli = wsas.result_dpli.data.avg_left_midline_dpli';
+avg_right_lateral_dpli = wsas.result_dpli.data.avg_right_lateral_dpli';
+avg_right_midline_dpli = wsas.result_dpli.data.avg_right_midline_dpli';
+
 figure;
-min_point = min([fp_dpli.left_midline fp_dpli.left_lateral, fp_dpli.right_midline, fp_dpli.right_lateral]);
-max_point = max([fp_dpli.left_midline fp_dpli.left_lateral, fp_dpli.right_midline, fp_dpli.right_lateral]);
+min_point = min([avg_left_midline_dpli avg_left_lateral_dpli, avg_right_midline_dpli, avg_right_lateral_dpli]);
+max_point = max([avg_left_midline_dpli avg_left_lateral_dpli, avg_right_midline_dpli, avg_right_lateral_dpli]);
 
 subplot(2,2,1);
-x_index = linspace(0,total_x,length(fp_dpli.left_midline));
-plot(x_index, fp_dpli.left_midline, 'LineWidth',3);
-title('dPLI Left Midline');
+x_index = linspace(0,total_x,length(avg_left_midline_dpli));
+plot(x_index, avg_left_midline_dpli, 'LineWidth',3);
+title(strcat(title_name, ' dPLI Left Midline'));
 ylim([min_point,max_point]);
 xlabel('Seconds');
 
 subplot(2,2,2);
-x_index = linspace(0,total_x,length(fp_dpli.right_midline));
-plot(x_index, fp_dpli.right_midline, 'LineWidth',3);
-title('dPLI Right Midline');
+x_index = linspace(0,total_x,length(avg_right_midline_dpli));
+plot(x_index, avg_right_midline_dpli, 'LineWidth',3);
+title(strcat(title_name, 'dPLI Right Midline'));
 ylim([min_point,max_point]);
 xlabel('Seconds');
 
 subplot(2,2,3);
-x_index = linspace(0,total_x,length(fp_dpli.left_lateral));
-plot(x_index, fp_dpli.left_lateral, 'LineWidth',3);
-title('dPLI Left Lateral');
+x_index = linspace(0,total_x,length(avg_left_lateral_dpli));
+plot(x_index, avg_left_lateral_dpli, 'LineWidth',3);
+title(strcat(title_name, ' dPLI Left Lateral'));
 ylim([min_point,max_point]);
 xlabel('Seconds');
 
 subplot(2,2,4);
-x_index = linspace(0,total_x,length(fp_dpli.right_lateral));
-plot(x_index, fp_dpli.right_lateral, 'LineWidth',3);
-title('dPLI Right Lateral');
+x_index = linspace(0,total_x,length(avg_right_lateral_dpli));
+plot(x_index, avg_right_lateral_dpli, 'LineWidth',3);
+title(strcat(title_name, ' dPLI Right Lateral'));
 ylim([min_point,max_point]);
 xlabel('Seconds');
 
 % Hub Location
-figure;
-x_index = linspace(0,total_x,length(hl_relative_position));
-plot(x_index, hl_relative_position, 'LineWidth', 3);
-title('HL Relative Position');
+figure
+hub_relative_position = result_hl.data.hub_normalized_value;
+x_index = linspace(0,total_x,length(hub_relative_position));
+plot(x_index, hub_relative_position, 'LineWidth', 3);
+title(strcat(title_name, ' HL Relative Position'));
 xlabel('Seconds');
+
 
 % Permutation Entropy
 figure;
-min_point = min([pe_frontal, pe_parietal]);
-max_point = max([pe_frontal, pe_parietal]);
+avg_permutation_entropy_anterior = result_pe.data.avg_permutation_entropy_anterior;
+avg_permutation_entropy_posterior = result_pe.data.avg_permutation_entropy_posterior;
+min_point = min([avg_permutation_entropy_anterior, avg_permutation_entropy_posterior]);
+max_point = max([avg_permutation_entropy_anterior, avg_permutation_entropy_posterior]);
 
 subplot(2,1,1);
-x_index = linspace(0,total_x,length(pe_frontal));
-plot(x_index, pe_frontal, 'LineWidth', 3);
-title('PE Frontal');
+x_index = linspace(0,total_x,length(avg_permutation_entropy_anterior));
+plot(x_index, avg_permutation_entropy_anterior, 'LineWidth', 3);
+title(strcat(title_name, ' PE Anterior'));
 ylim([min_point, max_point]);
 xlabel('Seconds');
 
 subplot(2,1,2);
-x_index = linspace(0,total_x,length(pe_parietal));
-plot(x_index, pe_parietal, 'LineWidth', 3);
-title('PE Parietal');
+x_index = linspace(0,total_x,length(avg_permutation_entropy_posterior));
+plot(x_index, avg_permutation_entropy_posterior, 'LineWidth', 3);
+title(strcat(title_name, ' PE Parietal'));
 ylim([min_point, max_point]);
 xlabel('Seconds');
