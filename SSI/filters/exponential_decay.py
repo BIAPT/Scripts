9d4f83682564f5d1672e73eb884fb17f978ba2ff
Bl0101 <yacine.mahdid@live.com>
2019-11-01 20:20:02 -0400

    camille's work :heart:

--- SSI/filters/exponential_decay.py
+++ SSI/filters/exponential_decay.py
@@ -10,8 +10,7 @@ def exponential_decay(input_data, output_data, alpha):
     Exponential Decay Smoothing Filter
     Increase alpha for more filtering: uses more past data to compute an average
     '''
-    print(alpha)
-    output_data[0] = input_data[0]
+    output_data[0] = input_data[0]; 
     for i in range(0,input_data.num-1):
         output_data[i+1] = (output_data[i,0]*alpha) + (input_data[i+1,0] * (1-alpha))
 
