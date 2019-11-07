1ddda0a1803ffdb2f8c48ce1d8903855fa06f7da
Bl0101 <yacine.mahdid@live.com>
2019-11-06 23:21:01 -0500

    refactor ruby :gem: script with added comments

--- SSI/tostream.rb
+++ SSI/tostream.rb
@@ -1,66 +1,83 @@
 #!/usr/bin/env ruby
+# Author: Parisa
+# Edit: Yacine
 #the final version I am working on
 
-role = "user"
-#role = "caregiver"
+# Get the timestamp of when the script was run 
+# We do this here so that all file created when this is run 
+# will have the same timestamp
+tnow = Time.now.strftime ("%Y/%m/%d %H:%M:%S:%L").to_s
+tutc = Time.now.getgm.strftime ("%Y/%m/%d %H:%M:%S:%L").to_s
 
+# Iterating through each TPS folder
+for tpsid in Dir["TP00*"]
+  puts "TPS id: " + tpsid
 
-for foldername in Dir["TP00*"]
-  #puts foldername
-  for filename in Dir[foldername + "/2019-*.csv"]
-    if !filename.include? "setting"
-      puts filename
-      filename2 = role + "." + filename[/.*\_(.*?)\_/,1] + ".stream~"
-      puts filename2
+  # Iterating through each files in the TPS folder
+  for filename in Dir[tpsid + "/2019-*.csv"]
 
-
-      #filename2=filename.sub(/csv$/,"stream~")
-      raise "big problem" if filename==filename2
-      f=File.open(filename,"r")
-      g=File.open(foldername+"/"+filename2, "w")
-      f.gets
-      rown = 0
-      while line=f.gets
-        line1 = line.split(/;/)[1,2].join(';')
-        line2 = line1.delete("\"").to_f
-        g.puts line2
-        rown = rown + 1
-      end
-      g.close
-      
-      
-      tnow = Time.now.strftime ("%Y/%m/%d %H:%M:%S:%L").to_s
-      tutc = Time.now.getgm.strftime ("%Y/%m/%d %H:%M:%S:%L").to_s
-      filename3=filename2.sub(/stream~/,"stream")
-      s=File.open(foldername+"/"+filename3,"w")
-
-      # Selecting right sample rate given the signal
-      if filename.include? "BVP"
-        samplerate = 300.0
-      elsif filename.include? "EDA" or filename.include? "TEMP" or filename.include? "HR"
-        samplerate = 15.0
-      elsif filename.include? "STR"
-        samplerate = -1.0 # What do we do with this?
-      else
-        raise "Filename doesn't include BVP, EDA, TEMP, HR or STR! Please double check if this code is still valid for your raw signals."
-      end
-
-
-      # Writing to the stream file
-      tab = "\t" # for readability
-      s.puts "<?xml version=\"1.0\" ?>"
-      s.puts "<stream ssi-v=\"2\">"
-      s.puts tab + "<info ftype=\"ASCII\" sr=\"" + samplerate.to_s + "\" dim=\"1\" byte=\"4\" type=\"FLOAT\" delim=\";\" />"
-      s.puts tab + "<meta />" # Not sure why we need this XML tag?
-      s.puts tab + "<time ms=\"0\" local=\"" + tnow + "\" system=\"" + tutc + "\"/>"
-      s.puts tab + "<chunk from=\"0.000000\" to=\"" + (rown/samplerate).to_s + "\" byte=\"0\" num=\"" + rown.to_s + "\"/>"
-      s.puts "</stream>"
-
-      # closing our file pointer
-      s.close
-      f.close
-
+    # skip the setting file
+    if filename.include? "setting"
+      next
     end
+
+    # Creating the Stream~ file (contianing the data)
+    puts "Input file: " + filename
+    streamname = tpsid + "." + filename[/.*\_(.*?)\_/,1] + ".stream~"
+    puts "Output file: " + streamname
+
+    # Some error checking
+    raise "big problem" if filename == streamname
+
+    # open our input and output file
+    rawfile = File.open(filename, "r")
+    streamfile = File.open(tpsid + "/" + streamname, "w")
+    
+    # Read the input file line by line 
+    # and copy the data in the stream~ file in the right format
+    rawfile.gets # skip the header of the input
+    rown = 0 # keep track of the number of row written for the stream file
+    while line = rawfile.gets
+
+      # Not sure why we need these two manipulation (the first one seems fine)
+      line = line.split(/;/)[1,2].join(';')
+      datapoint = line.delete("\"").to_f
+
+      streamfile.puts datapoint # write the number to the stream~ file
+      rown = rown + 1
+    end
+
+    # Close our file pointers for the input data and the stream~
+    streamfile.close
+    rawfile.close
+  
+    # Creating the header stream file (no tilda ~)
+    headername=streamname.sub(/stream~/,"stream")
+    headerfile = File.open(tpsid + "/" + headername,"w")
+
+    # Selecting right sample rate given the signal
+    if filename.include? "BVP"
+      samplerate = 300.0
+    elsif filename.include? "EDA" or filename.include? "TEMP" or filename.include? "HR"
+      samplerate = 15.0
+    elsif filename.include? "STR"
+      samplerate = -1.0 # What do we do with this?
+    else
+      raise "Filename doesn't include BVP, EDA, TEMP, HR or STR! Please double check if this code is still valid for your raw signals."
+    end
+
+    # Writing to the stream file
+    tab = "\t" # for readability
+    headerfile.puts "<?xml version=\"1.0\" ?>"
+    headerfile.puts "<stream ssi-v=\"2\">"
+    headerfile.puts tab + "<info ftype=\"ASCII\" sr=\"" + samplerate.to_s + "\" dim=\"1\" byte=\"4\" type=\"FLOAT\" delim=\";\" />"
+    headerfile.puts tab + "<meta />" # Not sure why we need this XML tag?
+    headerfile.puts tab + "<time ms=\"0\" local=\"" + tnow + "\" system=\"" + tutc + "\"/>"
+    headerfile.puts tab + "<chunk from=\"0.000000\" to=\"" + (rown/samplerate).to_s + "\" byte=\"0\" num=\"" + rown.to_s + "\"/>"
+    headerfile.puts "</stream>"
+
+    # closing our file pointer
+    headerfile.close
   end
 end
 
