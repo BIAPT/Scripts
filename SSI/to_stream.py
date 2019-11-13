953f4f27433cdb423ce5e38e04a39f559ee31140
Parisa Alirezaee <parisa.alirezaee@mail.mcgill.ca>
2019-11-13 13:39:17 -0500

    Update to_stream.py

--- SSI/to_stream.py
+++ SSI/to_stream.py
@@ -9,35 +9,62 @@ from os import listdir
 import datetime
 import time
 import glob
+import zipfile
+
 
 # Filters Imports
 import sys
 
+# #settnings for session Oct 31st
+# TPSDictionary = [
+# ["TPS001689" , "P1"],
+# ["TPS001491" , "P2"],
+# ["TPS001353" , "P3"],
+# ["TPS001123" , "P4"],
+# ["TPS001484" , "P5"],
+# ["TPS001376" , "P6"],
+# ["TPS001254" , "P7"],
+# ["TPS001822" , "P8"],
+# ["ABSENT" , "P9"],
+# ["ABSENT" , "P10"],
+# ["ABSENT" , "P11"],
+# ["TPS001472" , "P12"],
+# ["ABSENT" , "P13"]]
+# session = "Session_Oct_31"
+
+
+#mappings for session Nov 7th
+TPSDictionary = [
+["TPS001689" , "P1"],
+["ABSENT" , "P2"],
+["TPS001353" , "P3"],
+["ABSENT" , "P4"],
+["ABSENT" , "P5"],
+["TPS001376" , "P6"],
+["TPS001254" , "P7"],
+["TPS001822" , "P8"],
+["TPS001491" , "P9"],
+["TPS001354" , "P10"],
+["TPS001472" , "P11"],
+["TPS001884" , "P12"],
+["TPS001123" , "P13"]]
+session = "test_session"
+
+
+
 # Input and ouput paths (set these up so that it works with your computer)
-input_path = os.path.join("C:\\","Users","biomusic","Desktop","Session_Nov_7")
-output_path = os.path.join("C:\\","Users","biomusic","Desktop","Session_Nov_7_streams") #this should be changed to put straight in NOVA
+input_path = os.path.join("C:\\","Users","biomusic","Desktop", session)
+output_path = os.path.join("C:\\","Users","biomusic","Desktop","Nova", "data")
+# output_path = os.path.join("C:\\","Users","biomusic","Desktop","Nova", "only to test the python code")
 TPS1 = ""
 TPS2 = ""
+TPS3 = ""
+TPS4 = ""
 TPS1Dir = ""
 TPS2Dir = ""
+TPS3Dir = ""
+TPS4Dir = ""
 
-#to-do (Parisa)
-#Implement a way to map each TPS folder to the participant's ID number, so that it always put
-#all data from each participant together, even though they did not wear the same sensor
-#in each session
-
-TPSDictionary = [
-["TPS001491" , "P1"],
-["TPS001354" , "P2"],
-["TPS001689" , "P3"],
-["TPS001123" , "P4"],
-["TPS001472" , "P5"],
-["TPS001484" , "P6"],
-["TPS001822" , "P7"],
-["TPS001254" , "P8"],
-["TPS001376" , "P9"],
-["TPS001353" , "P10"],
-["TPS001884" , "P11"]]
 
 
 # Get the timestamp of when the script was run 
@@ -72,42 +99,85 @@ def pre_process(timestamps, data_pts, data_type, sample_rate):
 #and inside that folder are the recorded data from that phone.
 for colorfolder in listdir(input_path):
     color_path = input_path + os.sep + colorfolder
+    with zipfile.ZipFile(color_path, 'r') as zip_ref:
+        print("extracting... " + colorfolder)
+        zip_ref.extractall(input_path)
+        color_path = color_path.replace(".zip","")
     for sessionfolder in listdir(color_path):
         session_path = color_path + os.sep + sessionfolder
         for root, dirs, recordingfiles in os.walk(session_path):
             for filename in recordingfiles:
+                #calculate the number of sensors that are connected to that phone by reading the end of filename
+                #it can be 2 or 3 or 4 sensors, but you can always add more (not recommended)
+                if filename.endswith("4.csv"):
+                    TPS_to_one_phone = 4
+                elif filename.endswith("3.csv"):
+                    TPS_to_one_phone = 3
+                elif filename.endswith("2.csv"):
+                    TPS_to_one_phone = 2
+
                 #the recorded files are not named based on the TPS we got recording from
                 #however, the name of the TPSs are stored in session file
                 #so we have to read inside of it and create the folders with the corresponding TPS name
                 if "session" in filename:
                     sessionfile = open(session_path + os.sep + filename, "r")
                     contents =sessionfile.read()
-                    #for now, we only have two TPS connected to each phone
-                    #but this can be changed to support more
-                    TPS1 = "TPS" + contents.split("TP")[1].split(",")[0]
-                    TPS2 = "TPS" + contents.split("TP")[2].split(";")[0]
+                    if TPS_to_one_phone == 2:
+                        TPS1 = "TPS" + contents.split("TP")[1].split(",")[0]
+                        TPS2 = "TPS" + contents.split("TP")[2].split(";")[0]
+                        tpss = [TPS1,TPS2,"",""]
+
+                    elif TPS_to_one_phone == 3:
+                        TPS1 = "TPS" + contents.split("TP")[1].split(",")[0]
+                        TPS2 = "TPS" + contents.split("TP")[2].split(",")[0]
+                        TPS3 = "TPS" + contents.split("TP")[3].split(";")[0]
+                        tpss = [TPS1,TPS2,TPS3,""]
+
+                    elif TPS_to_one_phone == 4:
+                        TPS1 = "TPS" + contents.split("TP")[1].split(",")[0]
+                        TPS2 = "TPS" + contents.split("TP")[2].split(",")[0]
+                        TPS3 = "TPS" + contents.split("TP")[3].split(",")[0]
+                        TPS4 = "TPS" + contents.split("TP")[4].split(";")[0]
+                        tpss = [TPS1,TPS2,TPS3,TPS4]
+
+
                     for [TPSID,ParID] in TPSDictionary:
                         if TPSID == TPS1:
-                            TPS1Dir = os.path.join(output_path,ParID)
-                            print("Sensor: " + TPS1 + " on the phone color: " + colorfolder + " for participant: " + ParID)
+                            TPS1Dir = os.path.join(output_path,ParID,session)
+                            if not os.path.exists(TPS1Dir):
+                                os.mkdir(TPS1Dir)
+                            # print("Sensor: " + TPS1 + " on the phone color: " + colorfolder + " for participant: " + ParID)
                         if TPSID == TPS2:
-                            TPS2Dir = os.path.join(output_path,ParID)
-                            print("Sensor: " + TPS2 + " on the phone color: " + colorfolder + " for participant: " + ParID)
-                    
-
-                    if not os.path.exists(TPS1Dir):
-                        os.mkdir(TPS1Dir)
-                    if not os.path.exists(TPS2Dir):
-                        os.mkdir(TPS2Dir)
+                            TPS2Dir = os.path.join(output_path,ParID,session)
+                            if not os.path.exists(TPS2Dir):
+                                os.mkdir(TPS2Dir)
+                            # print("Sensor: " + TPS2 + " on the phone color: " + colorfolder + " for participant: " + ParID)
+                        if tpss[2] != "":
+                            if TPSID == TPS3:
+                                TPS3Dir = os.path.join(output_path,ParID,session)
+                                if not os.path.exists(TPS3Dir):
+                                    os.mkdir(TPS3Dir)
+                                # print("Sensor: " + TPS1 + " on the phone color: " + colorfolder + " for participant: " + ParID)
+                            if tpss[3] != "":
+                                if TPSID == TPS4:
+                                    TPS4Dir = os.path.join(output_path,ParID,session)
+                                    if not os.path.exists(TPS4Dir):
+                                        os.mkdir(TPS4Dir)
+                                # print("Sensor: " + TPS2 + " on the phone color: " + colorfolder + " for participant: " + ParID)
 
 
             for filename in recordingfiles:
                 #the recorded files end either with 1 or 2 which shows the file was recorded
                 #either from the first TPS on the session file or the second TPS
                 if filename.endswith("1.csv"):
-                    tps_dir = TPS1Dir
+                    participant_dir = TPS1Dir
                 elif filename.endswith("2.csv"):
-                    tps_dir = TPS2Dir
+                    participant_dir = TPS2Dir
+                elif filename.endswith("3.csv"):
+                    participant_dir = TPS3Dir
+                elif filename.endswith("4.csv"):
+                    participant_dir = TPS4Dir
+                    
 
                 if "BVP" in filename:
                     data_type = "BVP"
@@ -132,8 +202,8 @@ for colorfolder in listdir(input_path):
 
                 #open .stream and .stream~ output files with the naming that NOVA likes (role.type.stream)
                 stream_name = "user." + data_type + ".stream~"
-                stream_file = open(tps_dir + os.sep + stream_name, "w")
-                header_file = open(tps_dir + os.sep + stream_name.replace("stream~","stream"),"w")
+                stream_file = open(participant_dir + os.sep + stream_name, "w")
+                header_file = open(participant_dir + os.sep + stream_name.replace("stream~","stream"),"w")
                 print("Output file: " + stream_name)
                 print("Output file: " + stream_name.replace("stream~", "stream"))
 
