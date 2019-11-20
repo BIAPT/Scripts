1178b3a3fb53f617345eb5c42cb5d5ec57e9f92e
Bl0101 <yacine.mahdid@live.com>
2019-11-20 13:48:08 -0500

    add a fix for the colors of the feature weights

--- Amplitude and Phase Based Graph of Consciousness/experiments/experiment_6_plot_weights.m
+++ Amplitude and Phase Based Graph of Consciousness/experiments/experiment_6_plot_weights.m
@@ -1,40 +1,31 @@
 %% Variables initialization
-% Weights
-top = 0.2;
+%% Experiment variable
+analysis = 'aec'; % aec or wpli
+epoch = 'emf5'; % emf5 or eml5
+side = 'both'; % left right or both
 
-data = load('weights/wpli_pre_roc_weights.mat');
-weights_pli_pre_ROC = threshold(top, data.mean, data.std);
-
-data = load('weights/aec_pre_roc_weights.mat');
-weights_aec_pre_ROC = threshold(top, data.mean, data.std);
-
-data = load('weights/wpli_unconscious_weights.mat');
-weights_pli_unconscious = threshold(top, data.mean, data.std);
-
-data = load('weights/aec_unconscious_weights.mat');
-weights_aec_unconscious = threshold(top, data.mean, data.std);
+% Global variables
+data_folder = 'weights_data/';
+top = 0.2; % We can play around this
 
 % Colormap [nothing = 1, mean = 2, std = 3, both= 4]
-color_map = [ 1 1 1 ; 0.4660, 0.6740, 0.1880 ; 0.9290, 0.6940, 0.1250; 0, 0.4470, 0.7410]; % This is the colormap for the not important, mean, std, both
+color_map = [ 1 1 1 ; 0.9290, 0.6940, 0.1250; 0, 0.4470, 0.7410 ; 0.4660, 0.6740, 0.1880]; % This is the colormap for the not important, mean, std, both
 
-%% Weights vector initialization and USER INPUT
-weights = weights_pli_pre_ROC; %To change using the values above
 % Side of brain to map (left or right)
-side = 'both';
 if(strcmp(side,'left'))
    roi_range = 1:41;
 elseif(strcmp(side,'right'))
     roi_range = 42:82;
 else
     roi_range = 1:82;
-end
+end 
 
-%% Load the necessary variables
-load('ROI_MNI_V5_nii.mat'); % AALreg = volume image of 120 ROIs
-load('ROI_MNI_V5_vol.mat'); % ROI1 = contains info on ROIs
-load('Atlas_scouts.mat'); % scout = info of edited atlas 
+% Load the necessary variables
+load('../plot/ROI_MNI_V5_nii.mat'); % AALreg = volume image of 120 ROIs
+load('../plot/ROI_MNI_V5_vol.mat'); % ROI1 = contains info on ROIs
+load('../plot/Atlas_scouts.mat'); % scout = info of edited atlas
 
-%% Set the scouts name and labels
+% Set the scouts name and labels
 scout = scout(roi_range);
 
 number_locs = length(scout); % number_locs =  82
@@ -52,17 +43,38 @@ for jj = 1:length(scout)
     end
 end
 
-%% Fix the ROI
+% Fix the ROI
 roi = zeros([size(AALreg),number_locs]);
 for reg = 1:number_locs
     roi(:,:,:,reg) = AALreg == ROI1(labellist(reg)).ID;
 end
 
+% Load the data
+filename = strcat(data_folder,analysis,'_',epoch,'_weights.mat');
+data = load(filename);
+abs_weights = abs(data.weights);
+norm_weights = (abs_weights - min(abs_weights)) ./ (max(abs_weights) - min(abs_weights));
+t_weights = threshold(top, norm_weights);
+
+%% Weights vector initialization 
+
+% Side of brain to map (left or right)
+if(strcmp(side,'left'))
+   roi_range = 1:41;
+elseif(strcmp(side,'right'))
+    roi_range = 42:82;
+else
+    roi_range = 1:82;
+end 
+%% Plot the weights
+plot_weights_bar(analysis, norm_weights)
+
 %% Plot the brain with the correct weights
 % Can specify colour limits, here 0 to 1
-aal_brain(roi_range,weights,roi,color_map);
-camlight (90,30)
+aal_brain(roi_range,t_weights,roi,color_map);
+camlight (30,0)
 set(gca,'view',[270 90]); %Changes angle of view
+
 
 
 
@@ -91,15 +103,18 @@ function aal_brain(roi_range,weights,roi,color_map)
     set(gcf,'color','white')
 end
 
-function [weights] = threshold(top, weights_mean, weights_std)
-    all_weights = [weights_mean, weights_std];
-    all_weights = sort(all_weights);
+function [weights] = threshold(top, weights)
+    all_weights = sort(weights);
     top_index = floor(length(all_weights)*(1-top));
     weight_threshold = all_weights(top_index);
     
+    % Get the mean and std out of the weights vector
+    weights_mean = weights(1:82);
+    weights_std = weights(83:end);
+    
+    % Treshold the values
     weights_mean(weights_mean < weight_threshold) = 0;
     weights_mean(weights_mean >= weight_threshold) = 1;
-    
     weights_std(weights_std < weight_threshold) = 0;
     weights_std(weights_std >= weight_threshold) = 1;
 
@@ -118,5 +133,4 @@ function [weights] = threshold(top, weights_mean, weights_std)
            weights(i) = 1;
        end
     end
-    
 end
--- Amplitude and Phase Based Graph of Consciousness/plot/plot_weights_bar.m
+++ Amplitude and Phase Based Graph of Consciousness/plot/plot_weights_bar.m
@@ -5,13 +5,13 @@ function plot_weights_bar(type, weights)
     % Make a figure with subplot
     figure
     subplot(2,1,1)
-    bar(weights(1:84))
+    bar(weights(1:82))
     title(strcat(type, ' Weights of Mean'))
     xlim([0,83])
     ylim([-1.6,1.8])
 
     subplot(2,1,2)
-    bar(weights(84:end))
+    bar(weights(83:end))
     title(strcat(type, ' Weights of Std'))
     xlim([0,83])
     ylim([-1.6,1.8])
