29dfa391142e6d6aa30a6a16ea57556289ba212c
Bl0101 <yacine.mahdid@live.com>
2019-12-01 16:03:16 -0500

    add script for motif analysis testing

---
+++ Motif Analysis Augmented (Source,Target and Distance)/experiment_1_test_motif_3.m
@@ -0,0 +1,42 @@
+%{
+    Script written by Yacine Mahdid at 2019-12-01 for the review of the
+    motif analysis paper. 
+    The goal is to get the source, target and the average distance of the
+    motif connection.
+%}
+
+%% Using real data to test the motif_3.m script inside the NA library
+
+% Load the data whatever it is
+data_folder = '/test_data'; % where the data is
+filename = 'test_data.set'; % which data you want to run this analysis on
+[filepath,name,ext] = fileparts(mfilename('fullpath'));
+test_data_path = strcat(filepath,data_folder);
+recording = load_set(filename,test_data_path);
+
+% Variables initialization for dPLI calculation
+% dPLI
+frequency_band = [7 13]; % This is in Hz
+window_size = 10; % This is in seconds and will be how we chunk the whole dataset
+number_surrogate = 20; % Number of surrogate wPLI to create
+p_value = 0.05; % the p value to make our test on
+step_size = window_size;
+result_dpli = na_dpli(recording, frequency_band, window_size, step_size, number_surrogate, p_value);
+
+% Variables initialization for motif analysis
+network = make_phase_lead(result_dpli.data.avg_dpli);
+number_rand_network = 10;
+bin_swaps = 10;
+weight_frequency = 0.1;
+[intensity, coherence, frequency] = motif_3(network, number_rand_network, bin_swaps, weight_frequency);
+
+% Here we will normalize the output of this (only the frequency) and plot
+% it
+
+% We normalize by using the z score
+[norm_frequency] = normalize_motif(frequency);
+
+% Here we will plot a topographic map of the motif of interest
+for i = 1:13
+    plot_motif(norm_frequency(i,:),strcat("Motif ",string(i)," at alpha"),result_dpli.metadata.channels_location,'hot');
+end
--- Pain Recognition with EEG/plot/make_wpli.m
+++ Pain Recognition with EEG/plot/make_wpli.m
@@ -19,3 +19,15 @@ function make_wpli(data,type,labels)
     colormap(axe2, 'jet');
     colormap(axe3, 'hot');
 end
+
+% Helper function to create a matrix (for wpli and dpli)
+function make_matrix(data, title_name, labels)
+    imagesc(data);
+    xtickangle(90)
+    xticklabels(labels);
+    yticklabels(labels);  
+    xticks(1:length(labels));
+    yticks(1:length(labels));
+    colorbar;
+    title(title_name);
+end
