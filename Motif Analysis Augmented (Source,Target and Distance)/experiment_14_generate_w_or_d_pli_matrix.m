35bace53bfd814da300016cf43a2a7acc522eb69
Bl0101 <yacine.mahdid@live.com>
2020-01-18 15:31:33 -0500

    add error and tests for failure in the run_all_analysis script

---
+++ Motif Analysis Augmented (Source,Target and Distance)/experiment_14_generate_w_or_d_pli_matrix.m
@@ -0,0 +1,9 @@
+%{
+    Yacine Mahdid 2020-01-08
+    This script will calculate the wpli and the dpli matrices (at alpha)
+    that are needed to run the subsequent analysis. The parameters for the
+    analysis can be found in this script
+
+%}
+
+throw "not implemented"
