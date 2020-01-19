956736804a9df57e11a668303d05ddb1d84398b6
Bl0101 <yacine.mahdid@live.com>
2020-01-18 20:06:32 -0500

    refactoring and commenting the w/d pli experiment

--- Motif Analysis Augmented (Source,Target and Distance)/experiment_14_generate_w_or_d_pli_matrix.m
+++ Motif Analysis Augmented (Source,Target and Distance)/experiment_14_generate_w_or_d_pli_matrix.m
@@ -6,32 +6,33 @@
 
 %}
 
-%% Load the settings 
+%% Seting up the variables
 settings = load_settings();
 input_path = settings.input_path;
 output_path = settings.output_path;
 
-%% Experimental Variables
-
-% That might be a good idea to put in settings.txt
 participants = {'MDFA17'};
 states = {'BASELINE', 'IF5', 'EMF5', 'EML30','EML10','EML5', 'RECOVERY'};
 
-% for wPLI
+%% Experimental Variables
 frequency_band = [8 13]; % This is in Hz
 window_size = 10; % This is in seconds and will be how we chunk the whole dataset
 number_surrogate = 20; % Number of surrogate wPLI to create
 p_value = 0.05; % the p value to make our test on
 step_size = window_size;
 
+% Create the (w/d)pli directory
 wpli_output_path = mkdir_if_not_exist(output_path,'wpli');
 dpli_output_path = mkdir_if_not_exist(output_path,'dpli');
+% Iterate over the participants
 for p = 1:length(participants)
 
+    % Create the participants directory
     participant = participants{p};
     wpli_participant_output_path =  mkdir_if_not_exist(wpli_output_path, participant);
     dpli_participant_output_path =  mkdir_if_not_exist(dpli_output_path, participant);
 
+    % Iterate over the states
     for s = 1:length(states)
         state = states{s};
         
@@ -40,19 +41,14 @@ for p = 1:length(participants)
         data_location = strcat(input_path,filesep,participant);
         recording = load_set(raw_data_filename,data_location);
         
-        dpli_state_filename = strcat(dpli_participant_output_path,filesep,state,'_dpli.mat');
-        
-        % Calculate wpli and dpli
+        % Calculate wpli
         wpli_state_filename = strcat(wpli_participant_output_path,filesep,state,'_wpli.mat');
         result_wpli = na_wpli(recording, frequency_band, window_size, step_size, number_surrogate, p_value);
         save(wpli_state_filename, 'result_wpli');
         
+        % Calculate dpli
+        dpli_state_filename = strcat(dpli_participant_output_path,filesep,state,'_dpli.mat');
         result_dpli = na_dpli(recording, frequency_band, window_size, step_size, number_surrogate, p_value);
         save(dpli_state_filename, 'result_dpli');
     end
 end
-
-mkdir(output_path,'dpli');
-
-
-throw (MException('Motif:NotImplemented','not implemented'));
