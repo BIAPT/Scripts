bdac8bbfcb633541c372f1b7f5b58031388d43d8
Yacine Mahdid <yacine.mahdid@mail.mcgill.ca>
2020-02-14 16:55:24 -0500

    add csv + dummy input

--- Hub Location Method Analysis/utils/eeg_manipulation/reorder.py
+++ Hub Location Method Analysis/utils/eeg_manipulation/reorder.py
@@ -1,3 +1,5 @@
-
-def reorder_channels(input):
-    print(input)
+import numpy as np
+def reorder_channels(matrix, label, reorder_type):
+    print(matrix)
+    print(label)
+    print(reorder_type)
