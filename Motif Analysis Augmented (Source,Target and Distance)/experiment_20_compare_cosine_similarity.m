9be402f97279940837e2f3b948b68926fcc96051
Yacine <yacine.mahdid@mail.mcgill.ca>
2020-02-18 15:25:33 -0500

    fix bug to have equal length vector in motif

--- Motif Analysis Augmented (Source,Target and Distance)/experiment_20_compare_cosine_similarity.m
+++ Motif Analysis Augmented (Source,Target and Distance)/experiment_20_compare_cosine_similarity.m
@@ -13,7 +13,7 @@ num_motif = 13;
 motif_folder = '/home/yacine/Documents/motif_analysis_output/motif';
 
 %% Manual Variable to Change
-ppt_name_1 = 'MDFA03';
+ppt_name_1 = 'MDFA07';
 ppt_state_1 = 'EML10';
 
 ppt_name_2 = 'AVERAGE';
@@ -25,6 +25,9 @@ ppt_data_1 = data.result_motif;
 
 data = load(strcat(motif_folder,filesep,ppt_name_2,filesep,ppt_state_2,'_motif.mat'));
 ppt_data_2 = data.result_motif;
+
+%% Equalizing the number of data points
+[ppt_data_1, ppt_data_2] = equalize(ppt_data_1, ppt_data_2);
 
 message = strcat("Participant: ",ppt_name_1," at ", ppt_state_1, " against ", ppt_name_2, " at ", ppt_state_2);
 disp(message)
@@ -38,3 +41,55 @@ for m = 1:num_motif
     end
 end
 
+
+function [ppt_data_1, ppt_data_2] = equalize(ppt_data_1, ppt_data_2)
+    
+
+    % If both data structure have the same amount of channels
+    if (length(ppt_data_1.channels_location) == length(ppt_data_2.channels_location))
+       return 
+    end
+    
+
+    index_to_remove = [];
+    for p1_i = 1:length(ppt_data_1.channels_location)
+       label_1 = ppt_data_1.channels_location(p1_i).labels;
+
+       is_found = 0;
+       for p2_i = 1:length(ppt_data_2.channels_location)
+          label_2 = ppt_data_2.channels_location(p2_i).labels;
+
+          if(strcmp(label_1,label_2))
+             is_found = 1;
+             break;
+          end
+       end
+
+       if(is_found == 0)
+          index_to_remove = [index_to_remove, p1_i];
+       end
+    end
+    
+    ppt_data_1.frequency(:,index_to_remove) = [];
+    
+    index_to_remove = [];
+    for p2_i = 1:length(ppt_data_2.channels_location)
+       label_2 = ppt_data_2.channels_location(p2_i).labels;
+
+       is_found = 0;
+       for p1_i = 1:length(ppt_data_1.channels_location)
+          label_1 = ppt_data_1.channels_location(p1_i).labels;
+
+          if(strcmp(label_1, label_2))
+             is_found = 1;
+             break;
+          end
+       end
+
+       if(is_found == 0)
+          index_to_remove = [index_to_remove, p2_i];
+       end
+    end
+    
+    ppt_data_2.frequency(:,index_to_remove) = [];
+end
