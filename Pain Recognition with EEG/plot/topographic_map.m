8591b7e69a3d05318efdc513113878dc8eace760
Yacine Mahdid <yacine.mahdid@mail.mcgill.ca>
2020-03-09 15:52:07 -0400

    remove pain recognition (moved to its own repository)

--- Pain Recognition with EEG/plot/topographic_map.m
+++
@@ -1,7 +0,0 @@
-function topographic_map(data,location)
-    topoplot(data,location,'maplimits','absmax', 'electrodes', 'off');
-    min_color = min(data);
-    max_color = max(data);
-    caxis([min_color max_color])
-    colorbar;
-end
