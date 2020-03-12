dc8a021e05b4a19d7643bd0bf1f6e135f9837ba5
Charlotte <charlotte.maschke@gmx.de>
2020-03-12 15:09:36 -0400

    stabilitz

--- DOC_Clustering/compute_stability.py
+++ DOC_Clustering/compute_stability.py
@@ -6,6 +6,7 @@ import pandas as pd
 import stability_measure
 from matplotlib import pyplot as plt
 import random
+import pickle
 
 data=pd.read_pickle('data/final_wPLI_clustering.pickle')
 X=data.iloc[:,4:]
@@ -20,28 +21,59 @@ Stability Index
 """
 P=[3,4,5,6,7,8,9,10]     #number of Principal components to iterate
 K=[2,3,4,5,6,7,8,9,10]     #number of K-clusters to iterate
-Rep=10         #number of Repetitions (Mean at the end)
+Rep=100         #number of Repetitions (Mean at the end)
 
 [SI_M ,SI_SD] = stability_measure.compute_stability_index(X,Y_ID, P, K, Rep)
+
+
+pd.DataFrame(SI_M).to_pickle('SI_M_100x100x3-10x2-10.pickle')
+pd.DataFrame(SI_SD).to_pickle('SI_SD_100x100x3-10x2-10.pickle')
+
+
 
 P_l="".join(map(str, P))
 K_l="".join(map(str, K))
 
+plt.imshow(np.transpose(SI_M))
+plt.xticks(np.arange(10),('2','3','4','5','6','7','8','9','10'))
+plt.yticks(np.arange(9),('3','4','5','6','7','8','9','10'))
+plt.imshow(np.transpose(SI_M))
+plt.title('Stability Index Mean')
+plt.colorbar()
+plt.ylabel('Principle Components')
+plt.xlabel('K-Clusters')
+#plt.yticks(np.arange(len(K)),K_l)
+
+
+plt.imshow(np.transpose(SI_M[3:,:]))
+plt.xticks(np.arange(7),('5','6','7','8','9','10'))
+plt.yticks(np.arange(9),('3','4','5','6','7','8','9','10'))
+plt.imshow(np.transpose(SI_M))
+plt.title('Stability Index Mean')
+plt.colorbar()
+plt.clim(0.5,0.9)
+plt.ylabel('Principle Components')
+plt.xlabel('K-Clusters')
+#plt.yticks(np.arange(len(K)),K_l)
+
+plt.imshow(SI_M)
+plt.yticks(np.arange(10),('2','3','4','5','6','7','8','9','10'))
+plt.xticks(np.arange(9),('3','4','5','6','7','8','9','10'))
 plt.imshow(SI_M)
 plt.title('Stability Index Mean')
 plt.colorbar()
 plt.xlabel('Principle Components')
-plt.xticks(np.arange(len(P)),P_l)
 plt.ylabel('K-Clusters')
-plt.yticks(np.arange(len(K)),K_l)
+#plt.yticks(np.arange(len(K)),K_l)
 
-plt.imshow(SI_SD)
+plt.imshow(np.transpose(SI_SD))
+plt.xticks(np.arange(10),('2','3','4','5','6','7','8','9','10'))
+plt.yticks(np.arange(9),('3','4','5','6','7','8','9','10'))
+plt.imshow(np.transpose(SI_SD))
 plt.title('Stability Index SD')
-plt.xlabel('Principle Components')
-plt.xticks(np.arange(len(P)),P_l)
-plt.ylabel('K-Clusters')
-plt.yticks(np.arange(len(K)),K_l)
-
+plt.colorbar()
+plt.ylabel('Principle Components')
+plt.xlabel('K-Clusters')
 
 """
 Silhouette Score
@@ -54,6 +86,8 @@ X_temp=X_rest_step    #Template set (50% of Participants)
 X_test=X_anes_step    #Test set (50% of Participants)
 
 [SIL_M ,SIL_SD] = stability_measure.compute_silhouette_score(X_temp, X_test, P, K, Rep)
+
+
 
 
 P_l="".join(map(str, P))
