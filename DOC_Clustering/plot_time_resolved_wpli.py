738d88ffd9314f6e2a756cea3ad56c5f86029d3c
CharlotteMaschke <55599742+CharlotteMaschke@users.noreply.github.com>
2020-04-09 17:29:06 -0400

    visualization

--- DOC_Clustering/plot_time_resolved_wpli.py
+++ DOC_Clustering/plot_time_resolved_wpli.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from import_time_data_mean import *
 
-data=pd.read_pickle('final_wpli_all_NEW.pickle')
+data=pd.read_pickle('data/WSAS_TIME_DATA_250Hz/wPLI_10_1/final_wpli_all_NEW.pickle')
 
 data12_base=data.iloc[np.where((data['ID']=='12') & (data['Phase']=='base'))[0],:]
 data12_anes=data.iloc[np.where((data['ID']=='12') & (data['Phase']=='anes'))[0],:]
@@ -17,7 +17,6 @@ data20_reco=data.iloc[np.where((data['ID']=='20') & (data['Phase']=='reco'))[0],
 
 areas=['FC','FP','FO','FT','TO','TC','TP','PO','PC','CO',"Mean"]
 
-
 plt.subplot(311)
 plt.plot(data12_base['MEAN'])
 plt.hlines((np.mean(data12_base['MEAN'])),0,290)
@@ -25,22 +24,28 @@ plt.ylabel('wPLI')
 plt.title('WSAS_12 during Baseline')
 
 plt.subplot(312)
-plt.plot(np.mean(data12_anes.iloc[:,5:],axis=1))
-plt.hlines(np.mean(np.mean(data12_anes.iloc[:,5:])),0,290)
+plt.plot(np.mean(data12_anes.iloc[:,4:],axis=1))
+plt.hlines(np.mean(np.mean(data12_anes.iloc[:,4:])),0,290)
 plt.ylabel('wPLI')
 plt.title('WSAS_12 during Anesthesia')
 
-
 plt.subplot(313)
-plt.plot(np.mean(data12_reco.iloc[:,5:],axis=1))
-plt.hlines(np.mean(np.mean(data12_reco.iloc[:,5:])),0,290)
+plt.plot(np.mean(data12_reco.iloc[:,4:],axis=1))
+plt.hlines(np.mean(np.mean(data12_reco.iloc[:,4:])),0,290)
 plt.xlabel('timestep')
 plt.ylabel('wPLI')
 plt.title('WSAS_12 during Recovery')
 
+plt.subplot(211)
+plt.plot(data12_base.iloc[:,4:])
+#plt.hlines(np.mean(np.mean(data12_anes.iloc[:,4:])),0,290)
+plt.ylabel('wPLI')
+plt.title('WSAS_12 during Baseline')
+#plt.legend(areas)
 
-plt.plot(data12_anes.iloc[:,5:])
-plt.hlines(np.mean(np.mean(data12_anes.iloc[:,5:])),0,290)
+plt.subplot(212)
+plt.plot(data12_anes.iloc[:,4:])
+#plt.hlines(np.mean(np.mean(data12_anes.iloc[:,4:])),0,290)
 plt.ylabel('wPLI')
 plt.title('WSAS_12 during Anesthesia')
 plt.legend(areas)
