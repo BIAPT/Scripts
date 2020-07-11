correct stability index calculation

DOC_Clustering/clustering/compute_stability.py
@@ -48,7 +48,7 @@ fig,a =  plt.subplots(2,2)
 plt.setp(a, xticks=[0,1,2,3,4,5,6,7,8,9] , xticklabels=['2','3','4','5','6','7','8','9','10'],
         yticks=[0,1,2,3,4,5,6,7,8], yticklabels= ['3','4','5','6','7','8','9','10'],
          xlabel= 'K-Clusters',ylabel='Principle Components')
-im=a[0][0].imshow(np.transpose(SI_M))
+im=a[0][0].imshow(np.transpose(np.mean(SI,0)))
 a[0][0].set_title('Stability Index Mean: Baseline')
 plt.colorbar(im,ax=a[0,0])
 im=a[0][1].imshow(np.transpose(SI_SD))

DOC_Clustering/clustering/stability_measure.py
@@ -1,5 +1,7 @@
 '''
-This Script provides the methods is to calculate the stability index of a K-Means Clustering Solution (propoosed by Lange et al 2004)
+This Script provides the methods is to
+calculate the stability index of a K-Means
+Clustering Solution (propoosed by Lange et al 2004)
 '''
 import numpy as np
 from sklearn.cluster import KMeans
@@ -8,33 +10,29 @@ from scipy.spatial import distance
 from tqdm import tqdm
 from sklearn.metrics import silhouette_score
 import random
+from numpy import unravel_index
 
 def compute_stability_index(X,Y_ID,P,K,Rep):
 
-    SI=np.zeros([Rep,len(K) ,len(P)])   # Collection of stability index over Repetitions
-    x_complete = X  # complete input set for PCA-fit
+    SI = np.empty([Rep, len(K), len(P)])   # Collection of stability index over Repetitions
+    x_complete = X.copy() # complete input set for PCA-fit
 
-    for r in range(0,Rep):
+    for r in range(Rep):
         part = np.unique(Y_ID)
         nr_part = len(part)
-        rand = random.sample(range(0, nr_part), 2)  # Choose 2 elements
+        rand_temp = np.random.choice(part, nr_part // 2, replace=False)
+        rand_test = np.setdiff1d(part, rand_temp)
 
-        X_temp = X[(Y_ID == part[rand[0]]) | (Y_ID == part[rand[1]])]
-        X_test = X[(Y_ID != part[rand[0]]) & (Y_ID != part[rand[1]])]
-        """
-        X_temp = X[(Y_ID == part[rand[0]]) | (Y_ID == part[rand[1]]) | (Y_ID == part[rand[2]]) | (Y_ID == part[rand[3]]) | (
-                Y_ID == part[rand[4]])]
-        X_test = X[(Y_ID != part[rand[0]]) & (Y_ID != part[rand[1]]) & (Y_ID != part[rand[2]]) & (Y_ID != part[rand[3]]) & (
-                Y_ID != part[rand[4]])]
-        """
-        p_i = 0
+        X_temp = X[np.isin(Y_ID, rand_temp)]
+        X_test = X[np.isin(Y_ID, rand_test)]
+
         print('Repetition '+str(r)+'| '+str(Rep))
         for p in tqdm(P):
             pca = PCA(n_components=p)
             pca.fit(x_complete)
             X_temp_LD = pca.transform(X_temp) # get a low dimension version of X_temp
             X_test_LD = pca.transform(X_test) # and X_test
-            k_i=0
+
 
             for k in K:
                 kmeans = KMeans(n_clusters=k, max_iter=1000, n_init=100)
@@ -45,12 +43,25 @@ def compute_stability_index(X,Y_ID,P,K,Rep):
                 kmeans.fit(X_test_LD)           #fit the classifier on X_test
                 S_test = kmeans.predict(X_test_LD)
 
-                # proportion of disagreeing components in u and v
-                SI[r,k_i,p_i]=distance.hamming(S_test,S_temp) # should be already normalized
-                k_i=k_i+1
+                overlap = np.empty([k, k])
 
-            # increase p iteration by one
-            p_i=p_i+1
+                for c_test in range(k):
+                    for c_temp in range(k):
+                        # get common points of groups
+                        common = np.intersect1d(np.where(S_test == c_test),
+                                                np.where(S_temp == c_temp)).shape[0]
+                        overlap[c_test, c_temp] = common
+
+                common_val = 0
+
+                for i in range(k):
+                    maxval = unravel_index(overlap.argmax(), overlap.shape)
+                    common_val += overlap[maxval[0], maxval[1]]
+                    overlap[maxval[0], :] = -1
+                    overlap[:, maxval[1]] = -1
+
+                SI[r, K.index(k), P.index(p)] = 1-common_val/X.shape[0]
+
 
     SI_M=np.mean(SI,axis=0)
     SI_SD=np.std(SI,axis=0)